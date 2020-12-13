65cdd35b3a0a69ec0de5df008fddf9fe6ed74f41
ShivamKumarJha <jha.shivam3@gmail.com>
2020-12-13 19:47:15 +0530

    Add drivers/mmc/ modifications
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- drivers/mmc/core/sd.c
+++ drivers/mmc/core/sd.c
@@ -1210,6 +1210,7 @@ static void mmc_sd_detect(struct mmc_host *host)
 		goto out;
 	}
 
+	mmc_power_up(host, host->ocr_avail);
 	/*
 	 * Just check if our card has been removed.
 	 */
@@ -1318,6 +1319,7 @@ static int _mmc_sd_resume(struct mmc_host *host)
 		goto out;
 
 	if (host->ops->get_cd && !host->ops->get_cd(host)) {
+		err = -ENOMEDIUM;
 		mmc_card_clr_suspended(host->card);
 		goto out;
 	}
@@ -1349,6 +1351,9 @@ static int _mmc_sd_resume(struct mmc_host *host)
 		mmc_card_set_removed(host->card);
 		mmc_detect_change(host, msecs_to_jiffies(200));
 	} else if (err) {
+		pr_err("%s: %s: mmc_sd_init_card_failed (%d)\n",
+			mmc_hostname(host), __func__, err);
+		mmc_power_off(host);
 		goto out;
 	}
 	mmc_card_clr_suspended(host->card);
@@ -1417,14 +1422,16 @@ static int mmc_sd_runtime_suspend(struct mmc_host *host)
  */
 static int mmc_sd_runtime_resume(struct mmc_host *host)
 {
-	int err;
+	int err = 0;
 
 	err = _mmc_sd_resume(host);
-	if (err && err != -ENOMEDIUM)
+	if (err) {
 		pr_err("%s: error %d doing runtime resume\n",
 			mmc_hostname(host), err);
-
-	return 0;
+		if (err == -ENOMEDIUM)
+			mmc_card_set_removed(host->card);
+	}
+	return err;
 }
 
 static int mmc_sd_reset(struct mmc_host *host)
